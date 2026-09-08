1ae57ae846f0d4d38d9d7e39d09342ba3575c81a
Amro Meshref <amr.khamees@ejust.edu.eg>
2026-09-08 15:19:34 +0300

    update retrieve execution details tool

--- src/oshope/tools/retrieve_execution_details.py
+++ src/oshope/tools/retrieve_execution_details.py
@@ -1,5 +1,4 @@
 from oshope.core.states.oshope_state import CommandExecution, OSHopeState
-from oshope.config.config import PARALLEL_EXECUTION_ENABLED
 from oshope.utils.logger import get_logger
 from langchain.tools import Tool
 from typing import List
@@ -33,7 +32,6 @@ def command_executions_to_str(executions: List[CommandExecution]) -> str:
 def retrieve_execution_details(
     state: OSHopeState,
     step_index: int,
-    parallel_execution_enabled=PARALLEL_EXECUTION_ENABLED,
 ) -> str:
     """
     Retrieve the execution details of a specific command.
@@ -47,7 +45,7 @@ def retrieve_execution_details(
 
     try:
         command_executions = []
-        if parallel_execution_enabled:
+        if state.parallel_execution_enabled:
             for command_exe in state.planning.plan_steps[step_index].command_executions:
                 command_executions.append(command_exe)
         else:
